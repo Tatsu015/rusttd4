32cde4cb5725eb3a6483b271e09e920678731393
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-01-06 00:40:39 +0900

    base flow coded

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::time::{sleep, Duration};
+
+fn fetch() {
+    println!("Fetch program")
+}
+
+fn pc_up() {
+    println!("PC count up")
+}
+
+fn decode() {
+    println!("Decode instruction")
+}
+
+fn execute() {
+    println!("Execute instruction")
+}
+
+#[tokio::main]
+async fn main() {
+    loop {
+        fetch();
+        pc_up();
+        decode();
+        execute();
+        sleep(Duration::from_millis(1000)).await;
+    }
 }
