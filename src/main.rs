d9e328be10f9e2fdffdef3b908a8fe65ac9ba578
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-01-07 15:50:45 +0900

    add structs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,61 @@
 use tokio::time::{sleep, Duration};
 
-fn fetch() {
-    println!("Fetch program")
+type Register = u8;
+
+struct Cpu {
+    pc: Register,
+    carry: Register,
+    a: Register,
+    b: Register,
+    input: Register,
+    output: Register,
 }
 
-fn pc_up() {
-    println!("PC count up")
+impl Cpu {
+    fn fetch(&self) {
+        println!("Fetch program")
+    }
+
+    fn pc_up(&self) {
+        println!("PC count up")
+    }
+
+    fn decode(&self) {
+        println!("Decode instruction")
+    }
+
+    fn execute(&self) {
+        println!("Execute instruction")
+    }
 }
 
-fn decode() {
-    println!("Decode instruction")
+struct Emulator {
+    cpu: Cpu,
 }
 
-fn execute() {
-    println!("Execute instruction")
+impl Emulator {
+    fn run(&self) {
+        self.cpu.fetch();
+        self.cpu.pc_up();
+        self.cpu.decode();
+        self.cpu.execute();
+    }
 }
 
 #[tokio::main]
 async fn main() {
+    let cpu = Cpu {
+        pc: 0,
+        a: 0,
+        b: 0,
+        carry: 0,
+        input: 0,
+        output: 0,
+    };
+    let emulator = Emulator { cpu };
+
     loop {
-        fetch();
-        pc_up();
-        decode();
-        execute();
+        emulator.run();
         sleep(Duration::from_millis(1000)).await;
     }
 }
