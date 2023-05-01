345fa5fea8fd84bebcabe1c460af042164f5ca7c
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-05-02 00:20:10 +0900

    add arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use clap::Parser;
-use std::io;
+use clap::{Parser, Subcommand};
 
-use tokio::time::{sleep, Duration};
 extern crate env_logger as logger;
 extern crate log;
 use std::env;
@@ -11,34 +9,44 @@ mod emulator;
 mod register;
 mod rom;
 
-use crate::emulator::Emulator;
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    #[clap(about = "emulate td4 cpu")]
+    Emulate {
+        #[clap(long)]
+        program: String,
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    // execute program file name
-    #[arg(long, help = "specify execute program filepath")]
-    program: String,
+        #[clap(long, default_value_t = 1000)]
+        clock: u64,
+    },
+    #[clap(about = "compile binary file for td4 cpu emulator")]
+    Compile {
+        #[clap(long)]
+        src: String,
 
-    #[arg(
-        long,
-        default_value_t = 100,
-        help = "change clock speed in ms (default 100ms)"
-    )]
-    clock: u64,
+        #[clap(long)]
+        dst: String,
+    },
 }
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let args = Args::parse();
+#[derive(Parser, Debug)]
+#[clap(version = "1.0", about = "td4 cpu emulator")]
+struct Args {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+fn main() {
     env::set_var("RUST_LOG", "info");
     logger::init();
 
-    let mut emulator = Emulator::new(&args.program);
-    let clock = args.clock;
-
-    loop {
-        emulator.run();
-        sleep(Duration::from_millis(clock)).await;
+    let cli = Args::parse();
+    match cli.subcmd {
+        SubCommand::Emulate { program, clock } => {
+            println!("emulate program: {}, clock: {}", program, clock);
+        }
+        SubCommand::Compile { src, dst } => {
+            println!("compile src: {}, dst: {}", src, dst);
+        }
     }
 }
