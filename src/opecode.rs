36d43e77164d12f2c37b1894b60283858349e8ae
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-05-02 23:57:31 +0900

    add convert fn

--- src/opecode.rs
+++ src/opecode.rs
@@ -1,14 +1,34 @@
 pub enum Opecode {
-    AddA = 0x00,
-    MovAB = 0x01,
-    InA = 0x02,
-    MovA = 0x03,
-    MovBa = 0x04,
-    AddB = 0x05,
-    InB = 0x06,
-    MovB = 0x07,
-    OutB = 0x09,
-    Out = 0x0b,
-    Jnc = 0x0c,
-    Jmp = 0x0f,
+    AddA,
+    MovAB,
+    InA,
+    MovA,
+    MovBa,
+    AddB,
+    InB,
+    MovB,
+    OutB,
+    Out,
+    Jnc,
+    Jmp,
+}
+
+impl Opecode {
+    fn from(opecode: u8) -> Option<Self> {
+        match opecode {
+            0x00 => Some(Opecode::AddA),
+            0x01 => Some(Opecode::MovAB),
+            0x02 => Some(Opecode::InA),
+            0x03 => Some(Opecode::MovA),
+            0x04 => Some(Opecode::MovBa),
+            0x05 => Some(Opecode::AddB),
+            0x06 => Some(Opecode::InB),
+            0x07 => Some(Opecode::MovB),
+            0x09 => Some(Opecode::OutB),
+            0x0b => Some(Opecode::Out),
+            0x0c => Some(Opecode::Jnc),
+            0x0f => Some(Opecode::Jmp),
+            _ => None,
+        }
+    }
 }
