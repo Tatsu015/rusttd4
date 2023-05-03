5c49cf3764aea50df0bf9a393359ca27f438797e
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-05-03 19:41:43 +0900

    add empty code case

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -19,6 +19,9 @@ impl Token {
 }
 
 pub fn tokenize(code: &str) -> Result<Vec<Token>, String> {
+    if code == "" {
+        return Err("no code exist".to_string());
+    }
     let mnemonics: Vec<String> = code.lines().map(|l| l.trim().to_owned()).collect();
     let tokens: Result<Vec<_>, _> = mnemonics.iter().map(|m| to_token(&m)).collect();
     tokens.and_then(|v| Ok(v.into_iter().flatten().collect()))
@@ -50,28 +53,37 @@ fn to_token(mnemonic: &str) -> Result<Vec<Token>, String> {
     Ok(vec![opecode, operand])
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::tokenizer::{tokenize, Kind};
+#[cfg(test)]
+mod tests {
+    use crate::tokenizer::{tokenize, Kind};
 
-//     #[test]
-//     fn tokenize_2_mnimonic() {
-//         let tokens = tokenize("OUT 0011\nADD A,0001\n");
-//         assert_eq!(tokens.len(), 4);
-//         assert_eq!(tokens[0], Kind::Opecode);
-//         // assert_eq!(tokens[0].val, "OUT");
-//         // assert_eq!(tokens[1].kind, Kind::Operand);
-//         // assert_eq!(tokens[1].val, "0011");
-//         // assert_eq!(tokens[2].kind, Kind::Opecode);
-//         // assert_eq!(tokens[2].val, "ADD A");
-//         // assert_eq!(tokens[3].kind, Kind::Operand);
-//         // assert_eq!(tokens[3].val, "0001");
-//     }
+    #[test]
+    fn tokenize_2_mnimonic() {
+        let tokens = tokenize("OUT 0011\nADD A,0001\n").unwrap();
+        assert_eq!(tokens.len(), 4);
+        assert_eq!(tokens[0].val, "OUT");
+        assert_eq!(tokens[1].kind, Kind::Operand);
+        assert_eq!(tokens[1].val, "0011");
+        assert_eq!(tokens[2].kind, Kind::Opecode);
+        assert_eq!(tokens[2].val, "ADD A");
+        assert_eq!(tokens[3].kind, Kind::Operand);
+        assert_eq!(tokens[3].val, "0001");
+    }
 
-//     #[test]
-//     fn opecode_only() {
-//         let tokens = tokenize("OUT");
-//         let tokens = tokenize("INVALID");
-//         assert!(tokens.is_err());
-//     }
-// }
+    #[test]
+    fn invalid_mnemonic_syntax_by_opecode_only() {
+        let err = tokenize("OUT").is_err();
+        assert_eq!(err, true)
+    }
+    #[test]
+    fn invalid_mnemonic_syntax_by_operand_only() {
+        let err = tokenize("0011").is_err();
+        assert_eq!(err, true)
+    }
+
+    #[test]
+    fn invalid_mnemonic_syntax_by_empty() {
+        let err = tokenize("").is_err();
+        assert_eq!(err, true)
+    }
+}
